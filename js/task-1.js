c7ac0a6051f335b0d35b5ec36c6283bb7505b8fb
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-05-13 15:20:57 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,7 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
+  const makeCash = customerCredits - quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
